82c8c6c4143d06d162dbdb47f320194ab35ee2fc
Hien <ddhien98@gmail.com>
2024-06-15 16:38:51 +0700

    add: options for http client

--- src/shared/services/wish.service.ts
+++ src/shared/services/wish.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { WishItem } from '../models/wishItem';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,28 @@ export class WishService {
 
   constructor(private http: HttpClient) { }
 
+  private getStandardOptions(): any {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+  }
+
   getWishs() {
-    return this.http.get('wishs.json')
+    const options = this.getStandardOptions()
+    options.params = new HttpParams({
+      fromObject: {
+        format: 'json'
+      }
+    })
+
+    return this.http.get('wishs.json', options)
+  }
+
+  private addWish(wish: WishItem) {
+    const options = this.getStandardOptions()
+    options.headers.set('Authorization', 'value');
+    this.http.post('wishs.json', wish, options)
   }
 }
