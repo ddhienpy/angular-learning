71dac6fdc7a432c78fbb918452f5d5c592579746
Hien <ddhien98@gmail.com>
2024-06-15 16:29:17 +0700

    faeture: setup http client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgFor, NgIf } from '@angular/common';
 import { WishItem } from '../shared/models/wishItem';
@@ -7,13 +7,17 @@ import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
 import { EventService } from '../shared/services/EventService';
+import { WishService } from '../shared/services/wish.service';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, NgFor, NgIf, FormsModule, 
+    RouterOutlet,
+    NgFor,
+    NgIf,
+    FormsModule, 
     WishListComponent,
     AddWishFormComponent,
     WishFilterComponent
@@ -21,18 +25,21 @@ import { EventService } from '../shared/services/EventService';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-wishlist';
-  items: WishItem[] = [
-    new WishItem('Learn Angular'),
-    new WishItem('Get Coffee', true),
-    new WishItem('Buy Something'),
-  ]
+  items: WishItem[] = [];
 
-  constructor(private event: EventService) {
+  constructor(private event: EventService, private wishService: WishService) {
     this.event.listen('removeWish', (wish: any) => {
       console.log(wish)
       this.items.splice(this.items.indexOf(wish), 1)
+    })
+  }
+
+  ngOnInit(): void {
+    this.wishService.getWishs().subscribe((data: any) => {
+      console.log("data", data)
+      this.items = data
     })
   }
 
