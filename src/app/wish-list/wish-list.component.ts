604cad4d9d46e1a1193d85baa0e41e430293f093
Hien <ddhien98@gmail.com>
2024-06-15 16:00:02 +0700

    feature: custom wish list UI and handle display wish items

---
+++ src/app/wish-list/wish-list.component.ts
@@ -0,0 +1,15 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { WishItem } from '../../shared/models/wishItem';
+import { WishListItemComponent } from '../wish-list-item/wish-list-item.component';
+
+@Component({
+  selector: 'app-wish-list',
+  standalone: true,
+  imports: [NgFor, NgIf, WishListItemComponent],
+  templateUrl: './wish-list.component.html',
+  styleUrl: './wish-list.component.css'
+})
+export class WishListComponent {
+  @Input() wishs: WishItem[] = []
+}
