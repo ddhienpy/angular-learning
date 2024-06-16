67db8d5036a741bfb952f352719da310697ec0ba
Hien <ddhien98@gmail.com>
2024-06-16 22:47:53 +0700

    add: handle error for http client

--- src/shared/services/wish.service.ts
+++ src/shared/services/wish.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { WishItem } from '../models/wishItem';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -25,7 +26,17 @@ export class WishService {
       }
     })
 
-    return this.http.get('wishs.json', options)
+    return this.http.get('wishs.json', options).pipe(catchError(this.handleError))
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('Client Error: ', error.error)
+    } else {
+      console.error('Server Error: ', error.error)
+    }
+
+    return throwError(() => new Error('Error Message'))
   }
 
   private addWish(wish: WishItem) {
